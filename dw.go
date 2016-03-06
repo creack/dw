acaca25956822014f4dfae8fb4ea347cbf31c48b
Guillaume J. Charmes <gcharmes@leaf.ag>
2016-03-06 09:43:23 -0600

    Remove chans

--- dw.go
+++ dw.go
@@ -2,33 +2,22 @@ package dw
 
 // DoWhile holds the controls for the do...while().
 type DoWhile struct {
-	end   chan struct{}
-	start chan struct{}
-	cond  func() bool
+	hdlr func()
 }
 
 // Do simulate a do...while().
 func Do(hdlr func()) *DoWhile {
-	t := &DoWhile{
-		end:   make(chan struct{}),
-		start: make(chan struct{}),
+	return &DoWhile{
+		hdlr: hdlr,
 	}
-	go func() {
-		<-t.start
-		defer close(t.end)
-		for {
-			hdlr()
-			if !t.cond() {
-				break
-			}
-		}
-	}()
-	return t
 }
 
 // While starts the loop with the given condition.
 func (dw *DoWhile) While(cond func() bool) {
-	dw.cond = cond
-	close(dw.start)
-	<-dw.end
+	for {
+		dw.hdlr()
+		if !cond() {
+			break
+		}
+	}
 }
